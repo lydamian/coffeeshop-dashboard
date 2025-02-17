f3c5b1015684f7028bdc7a7c4c2ffa240ea54bd4
lydamian <lydamian@gmail.com>
2025-02-17 12:08:17 -0600

    working dashboard with the -03 stye, and working markdown integration with mdx

---
+++ src/app/recipes/espresso/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import Content from '@/app/recipes/espresso/content.mdx';
+
+export default function EspressoPage() {
+  return (
+    <div>
+      <Content />
+    </div>
+  );
+}
