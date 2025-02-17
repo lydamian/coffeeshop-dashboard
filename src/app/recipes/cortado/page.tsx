3e0945946002d0113ec666b798212e7a9bfbc9b8
lydamian <lydamian@gmail.com>
2025-02-17 17:49:30 -0600

    before codegening the recipes

---
+++ src/app/recipes/cortado/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import Content from '@/app/recipes/cortado/content.mdx';
+
+export default function CortadoPage() {
+  return (
+    <div className="prose my-12 mx-auto">
+      <Content />
+    </div>
+  );
+}
