cac3a149b1dba7c2b159f2fdae869249b932b435
lydamian <lydamian@gmail.com>
2025-02-17 12:22:52 -0600

    updates

--- src/app/recipes/espresso/page.tsx
+++ src/app/recipes/espresso/page.tsx
@@ -4,7 +4,7 @@ import Content from '@/app/recipes/espresso/content.mdx';
 
 export default function EspressoPage() {
   return (
-    <div>
+    <div className="prose my-12 mx-auto">
       <Content />
     </div>
   );
