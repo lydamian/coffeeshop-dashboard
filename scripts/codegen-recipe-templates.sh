f54286510227f5e35d56166de03042e55e2ee354
lydamian <lydamian@gmail.com>
2025-03-01 12:11:53 -0600

    added new recipe;s

--- scripts/codegen-recipe-templates.sh
+++ scripts/codegen-recipe-templates.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Define the source template folder
-TEMPLATE_FOLDER="./template"
+TEMPLATE_FOLDER="./src/app/recipes/template"
+
+# Define the parent directory where new folders will be created (same as template's parent directory)
+PARENT_DIR="$(dirname "$TEMPLATE_FOLDER")"
 
 # Define the list of new folder names
 NEW_FOLDERS=("simple-syrup" "vanilla-syrup" "black-sugar-syrup" "mixed-berry-syrup")
@@ -20,13 +23,14 @@ fi
 
 # Loop through the list of new folder names
 for folder in "${NEW_FOLDERS[@]}"; do
-    # Create the new folder if it doesn't exist
-    mkdir -p "$folder"
+    # Create the new folder in the same directory as the template folder
+    NEW_FOLDER_PATH="$PARENT_DIR/$folder"
+    mkdir -p "$NEW_FOLDER_PATH"
     
     # Copy the contents of the template folder to the new folder
-    cp "$TEMPLATE_FOLDER/content.mdx" "$TEMPLATE_FOLDER/page.tsx" "$folder/"
+    cp "$TEMPLATE_FOLDER/content.mdx" "$TEMPLATE_FOLDER/page.tsx" "$NEW_FOLDER_PATH/"
     
-    echo "Copied template contents to $folder"
+    echo "Copied template contents to $NEW_FOLDER_PATH"
 done
 
 echo "All folders created and populated successfully!"
